bcdc2e8370c325452cf74b26903ce7df333c4292
Zach Radloff <ZCR2244@live.com>
2024-06-15 14:40:56 +1000

    WIP: jest tests run

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,11 @@ module.exports = {
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
+  moduleNameMapper: {
+    "~/(.*)": "<rootDir>/src/electron/$1",
+    "@/(.*)": "<rootDir>/src/$1",
+    "@/applications(.*)": "<rootDir>/src/applications/$1",
+  },
   transform: {
     "^.+\\.(ts|tsx)$": [
       "ts-jest",
